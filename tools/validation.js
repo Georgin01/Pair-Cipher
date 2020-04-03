311dee10f1629469b99fd9c20e3f34fecb528c2a
Galay Yuriy <y4109galay@gmail.com>
2020-04-03 16:58:23 +0300

    create findUnique func for key

--- tools/validation.js
+++ tools/validation.js
@@ -9,6 +9,11 @@ function fullTrim(str) {
   return str.replace(/\s/g, '');
 }
 
+// Функция для выбора символов строки по 1. Используется для ключа
+function findUnique(str) {
+  return str.split('').filter((item, i, ar) => ar.indexOf(item) === i).join('');
+}
+
 export default function inputValidation(input) {
  if (typeof input !== 'object') {
    throw new Error('Invalid input. Something went wrong!');
@@ -18,9 +23,11 @@ export default function inputValidation(input) {
    throw new Error('Invalid keyword. Must be string and can only contain rus letters.');
  }
 
+ input.key = fullTrim(input.key).toLowerCase();
+
  return {
    text: input.msg,
-   key: fullTrim(input.key).toLowerCase(),
+   key: findUnique(input.key),
    flag: input.flag,
  };
 }
