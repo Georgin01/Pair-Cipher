7cbd91cfe6b1aedeab1f66b7007eeb111216ef04
Galay Yuriy <y4109galay@gmail.com>
2020-04-04 12:07:19 +0300

    add msg validation

--- tools/validation.js
+++ tools/validation.js
@@ -23,7 +23,12 @@ export default function inputValidation(input) {
    throw new Error('Invalid keyword. Must be string and can only contain rus letters.');
  }
 
+ if (typeof input.msg !== 'string' || !isCyrillic(fullTrim(input.msg))) {
+   throw new Error('Invalid message. Must be string and can only contain rus letters.');
+ }
+
  input.key = fullTrim(input.key).toLowerCase();
+ input.msg = input.msg.toLowerCase();
 
  if (findUnique(input.key).length !== 17) {
    throw new Error('Keyword doesn\'t have enough unique characters. Need 17');
@@ -32,6 +37,5 @@ export default function inputValidation(input) {
  return {
    text: input.msg,
    key: findUnique(input.key),
-   flag: input.flag,
  };
 }
